09c8c5d3cd6498e77250598e2d4ce5668a6a5171
William Magland <william.magland@gmail.com>
2021-03-15 22:13:27 -0400

    Updated

--- scripts/.startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-xrandr --output HDMI1 --auto --left-of eDP1 --primary
-wallpapername="Shore.jpg"
-feh --bg-fill ~/wallpapers/$wallpapername
-guake
-picom
-killall spotifyd
-spotifyd --config-path ~/.config/spotifyd/config --no-daemon
